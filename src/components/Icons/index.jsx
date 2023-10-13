17226f6f10a5ae9f559fcbbf1778349113b324b4
DOAcoder <dadahonovorifjon808@gmail.com>
2023-10-13 08:10:04 +0500

    travel

---
+++ src/components/Icons/index.jsx
@@ -0,0 +1,17 @@
+
+
+
+import Message from "./Message";
+import Speed from "./Speed";
+import Security from "./Security";
+import Like from "./Like";
+import Logo from "./Logo";
+import Map from "./Map";
+import Headphone from "./Headphone";
+import Convert from "./Convert";
+
+import Twitter from './Twitter';
+import Facebook from "./Facebook";
+import Insta from "./Insta";
+import YouTube from "./YouTube";
+export {Message, Speed, Security, Like, Logo, Map, Headphone, Convert, Twitter, Facebook, YouTube, Insta};
